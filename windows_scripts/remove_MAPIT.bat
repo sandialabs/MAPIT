6772e16c6ca67fa113bad6e99a2ee98f0e0d0eb0
nshoman <mr.roc12@gmail.com>
2023-01-30 13:15:50 -0500

    minor improvement
    removes downloaded conda installer now

--- windows_scripts/remove_MAPIT.bat
+++ windows_scripts/remove_MAPIT.bat
@@ -14,5 +14,6 @@ echo removing MAPIT environment...
 call conda remove --name MAPIT_env --all -y >nul 2>&1
 echo removing Miniconda3...
 rmdir /s /q %userprofile%\Miniconda3
+del .\Miniconda3-latest-Windows-x86_64.exe
 echo removal complete
 PAUSE
