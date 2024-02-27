a105f1a8e611a123ad87391b3031ebcdab164a6a
nshoman <mr.roc12@gmail.com>
2024-02-27 15:03:31 -0500

    updated docs

--- docs/_static/documentation_options.js
+++ docs/_static/documentation_options.js
@@ -1,6 +1,5 @@
-var DOCUMENTATION_OPTIONS = {
-    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.1.0-beta',
+const DOCUMENTATION_OPTIONS = {
+    VERSION: '1.3.25-beta',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
